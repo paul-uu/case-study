5d6e9cef8612fa3bfc213064758e8dc2403a10ee
Paul <pwittschen@gmail.com>
2015-08-26 00:04:43 -0400

    Update email_user.php

--- email_user.php
+++ email_user.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-	if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && filter_var(isset($_POST), FILTER_VALIDATE_EMAIL) ) {
+	if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && filter_var(isset($_POST['email']), FILTER_VALIDATE_EMAIL) ) {
 
 		$first_name = strip_tags(trim($_POST['first_name']));
 		$last_name = strip_tags(trim($_POST['last_name']));
