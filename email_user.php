f678e9b28858380504981b72e0c88599e56e7856
Paul <pwittschen@gmail.com>
2015-08-25 23:44:16 -0400

    Create email_user.php

---
+++ email_user.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && filter_var(isset($_POST), FILTER_VALIDATE_EMAIL) ) {
+
+		$first_name = strip_tags(trim($_POST['first_name']));
+		$last_name = strip_tags(trim($_POST['last_name']));
+		$email = strip_tags(trim($_POST['email']));
+
+		$message = $first_name . ",\n\nThanks for requesting a demo! We’ll get back to you soon.";
+		mail($email, 'Thanks for filling out that form.', $message);
+	}
+} 
+else {
+	http_response_code(500);
+}
+?>
